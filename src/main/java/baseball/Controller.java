99b7d887e2daa9ed9dad3cec3b212523a000ada2
soongjamm <kimstz0303@gmail.com>
2020-12-17 05:07:46 +0900

    feat: 랜덤 숫자 생성을 구현

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -2,5 +2,7 @@ package baseball;
 
 public class Controller {
     public void run() {
+        RandomNumberGenerator generator = new RandomNumberGenerator();
+        generator.generate().stream().forEach(x -> System.out.print(x + " "));
     }
 }
--- src/main/java/baseball/RandomNumberGenerator.java
+++ src/main/java/baseball/RandomNumberGenerator.java
@@ -1,6 +1,24 @@
 package baseball;
 
+import utils.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RandomNumberGenerator {
-    public void generate() {
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 9;
+    private static final int BASEBALL_NUMBERS_LENGTH = 3;
+
+    public List<Integer> generate() {
+        List<Integer> randomNumbers = new ArrayList<>();
+
+        while (randomNumbers.size() < BASEBALL_NUMBERS_LENGTH) {
+            int newNumber = RandomUtils.nextInt(START_INCLUSIVE, END_INCLUSIVE);
+            if (!randomNumbers.contains(newNumber)) {
+                randomNumbers.add(newNumber);
+            }
+        }
+        return randomNumbers;
     }
 }
